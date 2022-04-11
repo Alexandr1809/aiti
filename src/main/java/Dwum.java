0d2a27c203d5297c2091c19567fdc8f104269db3
Alexandr1809 <alexandrasp08@mail.ru>
2022-04-11 07:57:45 +0300

    "ARRAY"

---
+++ src/main/java/Dwum.java
@@ -0,0 +1,22 @@
+public class Dwum {
+    public void dhg(){
+        int[][] array =new int[3][4];
+        int[][] arra2={2,4,5},
+        {4,5,6},
+        {8,5,4};
+
+
+
+        for (int i=0;i<array.length;i++){
+
+            for (int j=0;j<array[i].length;j++){
+                array[i][j]=i+j;
+                System.out.println(array[int[j]]+ "");
+
+            }
+            System.out.println();
+        }
+
+
+    }
+}
---
+++ src/main/java/Skan.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Skan {
+    public void dhy(){
+
+     //   Scanner cs=new Scanner();
+       // int si=cs.nextInt();
+        //int[] ag= new int[];
+
+    }
+}
